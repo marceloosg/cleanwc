31fd527b69821a26d1f6a280f63f09d72839db79
marceloosg <marcelosg@gmail.com>
2016-05-23 02:48:52 -0300

    finished

--- run_analysis.R
+++ run_analysis.R
@@ -1,35 +1,47 @@
 library(plyr)
 library(data.table)
-#url="https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-#download.file(url,"./data.zip")
-#unzip("./data.zip")
+#0-Retrieving Raw Data
+
+datapath="UCI HAR Dataset"
+url=readLines("url",n=1)
+if (!file.exists(datapath)){
+        download.file(url,destfile="./data.zip",method="curl")
+        unzip("./data.zip")
+        dateDownloaded = date()
+}
+stopifnot(file.exists(datapath))
+
+#1-Extracting feature descriptive names
 features=data.table(read.csv("UCI HAR Dataset/features.txt",sep=" ",head=F))
+
+#2-Activity Descriptive names
 activity_labels=data.table(read.csv("UCI HAR Dataset/activity_labels.txt",sep=" ",head=F))
 colnames(activity_labels)=c("number","description")
 activity_labels$number=as.character(activity_labels$number)
 setkey(activity_labels,number)
 
-getdata=function(path){
-        file=paste0("UCI HAR Dataset/",path,"/X_",path,".txt")
-        lines=readLines(file)
-        data=data.table(ldply(lines, function(line){
-                line=sub("^ *","",line)
-                t(as.numeric(unlist(strsplit(gsub("  *",";",line),";"))))
-                }))
-        colnames(data)=as.character(features$V2)
-        
-        subject=read.csv(paste0("UCI HAR Dataset/",path,"/subject_",path,".txt"),sep=" ",head=F)
-        data$subject=as.integer(unlist(subject))
-        activities=as.character(unlist(read.csv(paste0("UCI HAR Dataset/",path,"/y_",path,".txt"),sep=" ",head=F)))
-        data$activities=as.character(activity_labels[activities]$description)
-        data
-}
+#Main  function: receives a string variable {path} as input that can be either "test"
+# or "train", and outputs a dataframe with the 561 features (as described in the "UCI HAR Dataset/feature.txt" file), plus
+# 2 columns: the first is of type integer identifying the subject (as described in the "UCI HAR Dataset/{path}/subject_{path}.txt")
+# the second column is the activity label obtained from the merging between the "UCI HAR Dataset/activity_labels.txt" and the
+# "UCI HAR Dataset/{path}/y_{path}.txt" file.
+source("get_data.R")
 
+#3-Extract test and train data into dataframes
 
-test=data.frame(getdata("test"),stringsAsFactors = F)
-train=data.frame(getdata("train"),stringsAsFactors = F)
+test=getdata("test")
+train=getdata("train")
+
+#3-Merge into one dataset
+
 merged=rbindlist(list(train,test))
+
+#4-Select only the relevant data from the merged dataset (mean and standards deviation of each measurements)
 selected=c(grep("(mean|std)\\.*.$",colnames(merged)),562,563)
 dt=merged[,selected,with=F]
+write.table(dt,"test_train.txt",sep="\t")
+
+#5-Summarize the dataset in a new dataset with the average for each subject and activity 
 dt_sub_act=dt[,lapply(.SD,mean,na.rm=T),by=list(subject,activities)]
+write.table(dt_sub_act,"averaged_features.txt",sep="\t")
 
